b6b1da00759530ceaa8bf24a3725097da4ca904d
Gaston Romero <Gastonhromero7@gmail.com>
2024-07-15 15:58:41 -0300

    Initial commit

---
+++ Proyecto/client/client.py
@@ -0,0 +1,2 @@
+from pymongo import MongoClient
+db_client = MongoClient("mongodb+srv://gastonhromero7:Messi41175282@cluster0.pu03jpc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").test
