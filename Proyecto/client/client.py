1838fce92fb40a0935a72e05294e7e950d2f6782
Gaston Romero <Gastonhromero7@gmail.com>
2024-07-16 12:07:41 -0300

    Initial commit

--- Proyecto/client/client.py
+++ Proyecto/client/client.py
@@ -1,2 +1,9 @@
 from pymongo import MongoClient
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
 db_client = MongoClient("mongodb+srv://gastonhromero7:Messi41175282@cluster0.pu03jpc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").test
+
+db_client = os.getenv("MONGO_DETAILS", "mongodb+srv://gastonhromero7:Messi41175282@cluster0.pu03jpc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = AsyncIOMotorClient(db_client)
+database = client.test
+user_collection = database.get_collection("users")
