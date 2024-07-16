ec07c055e2c9a7803ae9081e8ceea1318778207d
Gaston Romero <Gastonhromero7@gmail.com>
2024-07-16 12:52:51 -0300

    Login

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI,Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import os
+from routes import users,auth2
+
+
+app = FastAPI()
+app.include_router(users.rooter)
+app.include_router(auth2.root_login)
+
+static_path = os.path.join(os.path.dirname(__file__),'./static')
+templates_path = os.path.join(os.path.dirname(__file__),'./templates/')
+
+app.mount("/static",StaticFiles(directory=static_path),name='static')
+jina2 = Jinja2Templates(directory=templates_path)
+@app.get("/signup",response_class=HTMLResponse)
+async def root(request:Request):
+    return jina2.TemplateResponse('registro.html',{'request':request})
+
+@app.get("/login",response_class=HTMLResponse)
+async def root(request:Request):
+    return jina2.TemplateResponse('login.html',{'request':request})
