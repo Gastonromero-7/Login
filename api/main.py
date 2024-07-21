0131d16884c27249659957ed5c20e0312c281935
Gaston Romero <Gastonhromero7@gmail.com>
2024-07-21 19:41:29 -0300

    Login

--- api/main.py
+++ api/main.py
@@ -3,12 +3,12 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 import os
-from app.routes import users,auth2
-
+from app.routes.auth2 import root_login
+from app.routes.users import rooter
 
 app = FastAPI()
-app.include_router(users.rooter)
-app.include_router(auth2.root_login)
+app.include_router(rooter)
+app.include_router(root_login)
 
 static_path = os.path.join(os.path.dirname(__file__),'./static')
 templates_path = os.path.join(os.path.dirname(__file__),'./templates/')
